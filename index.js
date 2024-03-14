2574ddaeb3aee48260d7c801fcd8366cd14c564a
Steif <40876894+Again1013@users.noreply.github.com>
2024-03-14 17:55:15 +0800

    f

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import { genNodeAPI } from "arseeding-js";
-import { } from "fs";
+import { readdirSync, readFile } from "fs";
 
 const run = async () => {
-    const instance = genNodeAPI('YOUR PRIVATE KEY')
     const arseedUrl = 'https://arseed.web3infra.dev'
-    const data = Buffer.from('........')
     const payCurrencyTag = 'ethereum-usdt-0xdac17f958d2ee523a2206206994597c13d831ec7' // everPay 支持的 token tag (chainType-symbol-id)
-    // const options = {
-    //   tags: [{ name: 'Content-Type', value: 'image/png' }]
-    // }
-    // const res = await instance.sendAndPay(arseedUrl, data, payCurrencyTag, options)
-    // console.log('res', res)
+    const options = {
+        tags: [{name: 'Content-Type', value: 'image/png'}]
+    }
+    
+    const instance = genNodeAPI('YOUR PRIVATE KEY')
+    
+    const path = "../../Downloads/demoar/";
+    const files = readdirSync(path);
+    files.forEach(file => {
+        console.log("path:", path + file);
+        readFile(path + file, async (err, data) => {
+            
+            console.log(data);
+            const res = await instance.sendAndPay(arseedUrl, data, payCurrencyTag, options);
+            console.log('res', res);
+            
+        })
+    })
+    
+    
 }
-run()
+
+await run();
